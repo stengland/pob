da43eb69f74469891ec69f04bd83e37dd9e1f084
Liam  Richardson <liamrichardson@Macintosh-2.local>
2009-06-25 18:55:33 +0100

    feeds updated to be in one long list

--- pob-app.rb
+++ pob-app.rb
@@ -9,18 +9,34 @@ require 'rubygems'
 require 'sinatra'
 require 'feedzirra'
 
+class String
+  
+  def to_time(form = :utc)
+    ::Time.send("#{form}_time", *::Date._parse(self, false).values_at(:year, :mon, :mday, :hour, :min, :sec).map { |arg| arg || 0 })
+  end
+  
+end
 
 configure do
   # Configure stuff here you'll want to
   # only be run at Heroku at boot
 
-  feed_urls = ["http://feeds.feedburner.com/PaulDixExplainsNothing", "http://feeds.feedburner.com/trottercashion"]
+  feed_urls = [
+    "http://feeds2.feedburner.com/slashfilm",
+    "http://twitter.com/statuses/user_timeline/10535832.rss",
+    "http://api.flickr.com/services/feeds/photos_public.gne?id=85694872@N00&lang=en-us&format=atom"
+  ]
   Feeds = Feedzirra::Feed.fetch_and_parse(feed_urls)
 end
 
 # Quick test
 get '/' do
   updated_feeds = Feedzirra::Feed.update(Feeds.values)
+  entries = []
   
-  erb :index, :locals => {:feeds => updated_feeds}
+  updated_feeds.each{ |f| entries = entries + f.entries }
+  
+  entries.sort!{ |a,b| b.published.to_time <=> a.published.to_time  }
+  
+  erb :index, :locals => {:entries => entries}
 end
